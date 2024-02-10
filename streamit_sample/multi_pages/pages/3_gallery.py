6f322d16b6a27415b50fbf9700e941e94a6b8853
gbox3d <gbox3d@gmail.com>
2024-02-10 12:30:30 +0900

    multi page sample

---
+++ streamit_sample/multi_pages/pages/3_gallery.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import time
+import numpy as np
+
+st.set_page_config(page_title="About", page_icon="📈")
+
+st.markdown("# About")
+st.sidebar.header("About Page")
+st.image("https://static.streamlit.io/examples/cat.jpg")
